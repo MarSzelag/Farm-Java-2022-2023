515142d3d0138f579bee69e2c8a1752c7ea84788
Marcin Szeląg <marcin.szelag83@gmail.com>
2023-04-17 01:24:10 +0200

    Farm: currentTime() added, possessionsPlants() updated. Sown: addToSown(Plant plant, Farm farm) bug removed. minusWeekToHarvest(Farm farm) refactored to harvestTime(Farm farm) and added new method minusWeekToHarvest()

--- src/plants/SpringBarley.java
+++ src/plants/SpringBarley.java
@@ -2,6 +2,6 @@ package plants;
 
 public class SpringBarley extends Plant{
     public SpringBarley() {
-        super("Jęczmień jary", 270, 3300, 2, 15, 3, 0.95, 1.12, 252.00, 199.99);
+        super("Jęczmień jary", 270, 3300, 9, 15, 20, 0.95, 1.12, 252.00, 199.99);
     }
-} //TODO harvestTimeAfterNoOfWeeks = 20, sowingTimeFromWeek = 9
+}
